ce2631d4a58ae12af448b49f9e38110a8588e365
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2023-11-30 16:37:11 +0530

    Update

--- database/migrations/2023_11_29_113020_create_tasks_table.php
+++ database/migrations/2023_11_29_113020_create_tasks_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->date('due_date');
+            $table->integer('status')->default(0); 
             $table->timestamps();
         });
     }
